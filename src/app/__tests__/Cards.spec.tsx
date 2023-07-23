6674a0fdc6873a67b4adf687838a4012c956a5cb
Monica Vaz <monica.vaz@eumedicoresidente.com>
2023-07-23 14:23:48 -0300

    refactor: tests

--- src/app/__tests__/Cards.spec.tsx
+++ src/app/__tests__/Cards.spec.tsx
@@ -3,28 +3,38 @@ import { Cards } from '@/app/Cards';
 import '@testing-library/jest-dom';
 
 describe('Cards', () => {
-  it('should render the correct number of cards', async () => {
+  it('should initally render with no cards on screen', async () => {
     render(<Cards />);
     const input = screen.getByLabelText('Number of cards:');
 
     expect(input).toHaveValue('');
     expect(screen.queryAllByTestId('card')).toHaveLength(0);
+  });
 
+  it('should render cards accordingly to input change', async () => {
+    render(<Cards />);
+    const input = screen.getByLabelText('Number of cards:');
     fireEvent.change(input, { target: { value: '10' } });
 
     expect(input).toHaveValue('10');
     expect(screen.queryAllByTestId('card')).toHaveLength(10);
   });
 
-  it('should render card texts correctly', async () => {
+  it('should initally render all cards with the same text', async () => {
+    render(<Cards />);
+    const input = screen.getByLabelText('Number of cards:');
+    fireEvent.change(input, { target: { value: '4' } });
+
+    expect(screen.queryAllByText('down')).toHaveLength(4);
+    expect(screen.queryByText('up')).not.toBeInTheDocument();
+  });
+
+  it('should have a single card with up text when clicked', async () => {
     render(<Cards />);
     const input = screen.getByLabelText('Number of cards:');
     fireEvent.change(input, { target: { value: '4' } });
 
     const cards = screen.queryAllByTestId('card');
-    expect(cards.length).toBe(4);
-    expect(screen.queryAllByText('down')).toHaveLength(4);
-    expect(screen.queryByText('up')).not.toBeInTheDocument();
 
     fireEvent.click(cards[0]);
     expect(screen.queryAllByText('down')).toHaveLength(3);
